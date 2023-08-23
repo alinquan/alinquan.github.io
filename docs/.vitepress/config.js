13e9d7768550863fa5d197afe0248bdf17987286
林铨 <15521012174@163.com>
2023-08-23 14:19:02 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  title: "请叫我靓仔", // 网站标题
+  description: "总结归纳学习中的知识", // 网站的描述
+  base: "/", //  部署时的路径 默认 / ，使用二级地址 /base/
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]], // 添加网站图标 // 主题配置
+  themeConfig: {
+    logo: "/logo.jpg",
+    // 导航栏配置
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "书签", link: "/guide/" },
+      { text: "笔记", link: "/guide/" },
+      // {
+      //   text: "HTML/CSS",
+      //   items: [
+      //     { text: "HTML5", link: "/htmldocs/" },
+      //     { text: "CSS3", link: "/cssdocs/" },
+      //   ],
+      // },
+      // { text: "JavaScript", link: "/jsdocs/" },
+      // { text: "关于作者", link: "" },
+    ], //  左侧导航栏配置
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2019-present Evan LiangZai",
+    },
+    sidebar: {
+      htmldocs: [
+        {
+          text: "HTML5",
+          children: [
+            { text: "概况", link: "/htmldocs/" },
+            { text: "入门", link: "/htmldocs/htmlone" },
+            { text: "进阶", link: "/htmldocs/htmltwo" },
+          ],
+        },
+      ],
+      cssdocs: [
+        {
+          text: "css3",
+          children: [
+            { text: "css文档", link: "/cssdocs/" },
+            { text: "css文档1", link: "/cssdocs/cssone" },
+          ],
+        },
+      ],
+    },
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+layout: home
+hero:
+name: 靓仔
+text: 个人博客
+tagline: 标语
+actions: - theme: brand
+text: 开始
+link: /guide/what-is-vitepress - theme: alt
+text: View on GitHub
+link: https://github.com/vuejs/vitepress
+features:
+
+- icon: ⚡️
+  title: vite 超快冷启动和热加载
+  details: Lorem ipsum...
+- icon: 🖖
+  title: Vue 的力量与 Markdown 相遇
+  details: Lorem ipsum...
+- icon: 🛠️
+  title: 始终简单、最少
+  details: Lorem ipsum...
+
+<style>
+:root {
+  --vp-home-hero-name-color: transparent;
+  --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #bd34fe, #41d1ff);
+}
+</style>
